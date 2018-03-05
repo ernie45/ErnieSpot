94503ffadd3ce7d5cb0edc2de4531c4ebb4df53a
Ernesto Hernandez <h_ernesto@ymail.com>
2018-03-05 00:24:00 -0800

    different navigation

--- router.js
+++ router.js
@@ -1,9 +1,7 @@
 const path = require("path");
 
 module.exports = function(app){
-    app.get("/", function(req, res){
-        res.sendFile(path.resolve(__dirname, "./views/index.html"));
-    });
+    
     app.get("/contact", function(req, res){
         res.render("contact");
     });
