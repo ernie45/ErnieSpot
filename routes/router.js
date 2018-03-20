bcd484725a647b75fb7802512fc155fb95f24cb4
Ernesto Hernandez <h_ernesto@ymail.com>
2018-03-19 23:09:33 -0700

    trying stuff

--- routes/router.js
+++ routes/router.js
@@ -5,23 +5,20 @@ module.exports = function(app){
     // app.post("/send.php", function(req, res){
     //     res.sendFile(path.join(__dirname, "/public/send.php"));
     // });
-    app.get("/", function(req, res){
-      res.sen
-    });
-    app.post("/sendEmail", function(req, res){
-      console.log(req.body);
-      var message = req.body.message;
-      var sender = req.body.sender;
-      const sgMail = require('@sendgrid/mail');
-      sgMail.setApiKey(config.apiKey);
-      const msg = {
-        to: config.toEmail,
-        from: config.fromEmail,
-        subject: "About Ernie's Spot",
-        text: message,
-        html: "Sender: " + sender + "\nmessage: " + message,
-      };
-      sgMail.send(msg);
-      res.sendFile(path.join(__dirname, "../public/index.html"));
-    });
+    // app.post("/sendEmail", function(req, res){
+    //   console.log(req.body);
+    //   var message = req.body.message;
+    //   var sender = req.body.sender;
+    //   const sgMail = require('@sendgrid/mail');
+    //   sgMail.setApiKey(config.apiKey);
+    //   const msg = {
+    //     to: config.toEmail,
+    //     from: config.fromEmail,
+    //     subject: "About Ernie's Spot",
+    //     text: message,
+    //     html: "Sender: " + sender + "\nmessage: " + message,
+    //   };
+    //   sgMail.send(msg);
+    //   res.sendFile(path.join(__dirname, "../public/index.html"));
+    // });
 };
