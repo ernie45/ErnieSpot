3e66a74e377783baeb3da3ea3d6f081431e3610b
Ernesto Hernandez <h_ernesto@ymail.com>
2018-03-19 23:45:58 -0700

    added config file

---
+++ public/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    toEmail: "h_ernesto@ymail.com",
+    fromEmail: "hernandeernesto408@gmail.com",
+    apiKey: "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"
+}
